eaad552eb981d0698301b2e97a55455c5840ac43
Neto Ukpong-Neto <ukpongneto@wellahealth.com>
2021-03-27 15:22:06 +0100

    media updated

--- src/components/sections/Donate.js
+++ src/components/sections/Donate.js
@@ -109,13 +109,13 @@ const Donate = () => {
 We are working on opening doors to equal educational opportunities for children who live in poverty and also to give girls an equal opportunity at getting an education.
 Our activities are effective long-term investments that will help provide quality educational opportunities for bright young minds and young adults. This enables them to rise above poverty and become better decision makers.
             </p>
-            <p className="lead">
+            {/* <p className="lead">
             By providing these educational opportunities we decrease the number of potential young people falling through the cracks of the system. They will be less likely to walk through destructive paths that harm communities and perpetuate poverty.
             </p>
             <p className="lead">
             The funds help cover tuition, books, school supplies, uniforms, and other school-related costs.  By providing a scholarship to an African student, we offer a promise to the student that what they do in school has meaning, and they will have the reinforcement they need. Scholarships allow for future educational planning, and continuous support.
 Education is important because it is the glue that holds our society together. It is the step-up necessary to see above what is around you. Education provides guidance towards success as well as weapons against poverty. Education empowers women are able to break from oppressive gender roles and contribute more significantly to society. Education can mean the difference between a healthy long life or an unfortunately short one.
-            </p>
+            </p> */}
           </Media.Body>
           <img src={require('../images/foundation_1.png').default} alt="Education" height='300' width='300'/>
         </Media>
@@ -128,13 +128,13 @@ Education is important because it is the glue that holds our society together. I
             <p className="lead">
             The absence of an industrial revolution on the African subcontinent has left it with socio-economic structures that are, in the main, degenerative rather than accumulative’. Inequality in Africa health care system has risen significantly in the last 30 years. The number of  African countries living below the poverty line has increased e.g Nigeria from 19 million in 1970 to 90 million in 2000. In 1970, the top 2% of the population earned the same as the bottom 17%. By 2000 the income of the top 2% was equal to the bottom 55%.
             </p>
-            <p className="lead">
+            {/* <p className="lead">
             Technology is transforming how health care is delivered in Africa, giving more people in remote areas there and around the world access to better care. Likewise, easier access to data helps both doctors and policymakers make better-informed decisions about how to continue to improve the system. Even with these strides, however, the continent’s health care system faces big challenges. Our report finds that the private sector already delivers about half of Africa's health products and services. It calls for a close partnership between the public and private sectors, including improvements to regulatory oversight of private health care, and outlines ways that the private sector could be better engaged to improve its sustainability.
             </p>
             <p className="lead">
             Rather than serving only the rich, in Africa today the private sector is sometimes the only option for health care in many rural areas and poor urban slums. A poor woman in the region is as likely to take her sick child to a private hospital or clinic as to a public facility. 
 Our goal is to render basic health support assistance tru private healthcare services with standard healthcare services to the less vulnerable and privileged.
-            </p>
+            </p> */}
           </Media.Body>
         </Media>
         {/* Next Section */}
