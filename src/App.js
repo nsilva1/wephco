4b7dba2ec9e9052b11a313bab40ce5df6be9a64a
Neto Ukpong-Neto <ukpongneto@wellahealth.com>
2021-04-20 14:13:27 +0100

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // components
-import Landing from './components/sections/Landing';
-import Navbar from './components/common/Nav';
-import Footer from './components/common/Footer';
-import InteriorDesign from './components/services/InteriorDesign';
-import ExteriorDesign from './components/services/ExteriorDesign';
-import RealEstate from './components/services/RealEstate';
-import EventPlanning from './components/services/EventPlanning';
-import BureauDeChange from './components/services/BureauDeChange';
-import Consultants from './components/services/Consultants';
-import Donate from './components/sections/Donate'
+// import Landing from './components/sections/Landing';
+// import Navbar from './components/common/Nav';
+// import Footer from './components/common/Footer';
+// import InteriorDesign from './components/services/InteriorDesign';
+// import ExteriorDesign from './components/services/ExteriorDesign';
+// import RealEstate from './components/services/RealEstate';
+// import EventPlanning from './components/services/EventPlanning';
+// import BureauDeChange from './components/services/BureauDeChange';
+// import Consultants from './components/services/Consultants';
+// import Donate from './components/sections/Donate'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -30,10 +30,10 @@ import Hotel from './pages/Hotel/Hotel';
 function App() {
 	return (
 		<BrowserRouter>
-			<Navbar />
+			{/* <Navbar /> */}
 			<Switch>
-				<Route exact path='/' component={Landing} />
-				<Route exact path='/services/interior-design' component={InteriorDesign} />
+				<Route exact path='/' component={Hotel} />
+				{/* <Route exact path='/services/interior-design' component={InteriorDesign} />
 				<Route exact path='/services/exterior-design' component={ExteriorDesign} />
 				<Route exact path='/services/real-estate' component={RealEstate} />
 				<Route exact path='/services/event-planning' component={EventPlanning} />
@@ -41,9 +41,9 @@ function App() {
 				<Route exact path='/services/consultants' component={Consultants} />
 				<Route exact path='/contact' component={Consultants} />
 				<Route exact path='/foundation-home' component={Donate} />
-				<Route exact path='/hotel-booking' component={Hotel} />
+				<Route exact path='/hotel-booking' component={Hotel} /> */}
 			</Switch>
-			<Footer />
+			{/* <Footer /> */}
 		</BrowserRouter>
 	);
 }
